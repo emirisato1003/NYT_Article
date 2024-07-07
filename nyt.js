dc8e87c8116b21e3a70650085c03b13b43d15ff2
emirisato1003 <e.sato@students.clark.edu>
2024-07-07 16:15:31 -0700

    updated some stuff

--- nyt.js
+++ nyt.js
@@ -1,59 +1,64 @@
 /* script.js */
 const articleSection = document.getElementById('article-section');
 const result = document.querySelector('#article');
-let dataArticle = document.querySelector('[data-article]');
-let date = document.querySelector('#currentTime');
+const dataArticle = document.querySelector('[data-article]');
+const date = document.querySelector('#currentTime');
 
 // function to get nyt section for select form
 const getNytSection = (data) => {
     // console.log(data);
-    let output = '<option value="">Select a Section</option>';
-    for (let i = 1; i < data.results.length; i++) {
-        output += `<option value="${data.results[i].section}">${data.results[i].display_name}</option>`;
-    }
-    document.querySelector("[data-article-section]").innerHTML = output;
+    const options = data.results.slice(1).map(result =>
+        `<option value="${result.section}">${result.display_name}</option>`
+    );
+    document.querySelector("[data-article-section]").innerHTML = '<option value="#">Top News</option>' + options.join('');
 };
 
 // function to get selected article from select form
 const getSelectedSectionArticle = (data) => {
-    // console.log(data);
-    for (let i = 0; i < 7; i++) {
-        // check if there is image || title || abstract
-        if (data.results[i].multimedia[2].url || data.results[i].title || data.results[i.abstract]) {
-            let newsTitle = document.querySelector('#newsTitle-' + i);
-            newsTitle.href = data.results[i].url;
-            newsTitle.innerText = data.results[i].title;
+    const sectionTitle = document.getElementById('sectionTitle');
+    sectionTitle.innerHTML = `<h1>${data.results[0].section}</h1>`;
+    // new way
+    data.results.slice(0, 7).forEach((result, i) => {
+        // console.log(result)
+        if (result.multimedia?.[2]?.url || result.title || result.abstract) {
+            const newsTitle = document.querySelector(`#newsTitle-${i}`);
+            newsTitle.href = result.url;
+            newsTitle.innerHTML = result.title;
 
-            // document.querySelector('.titleSection-' + i).innerText = data.results[i].section;
-            document.querySelector('.titleSection-' + i).classList.add('d-none');
-            let img = document.querySelector('#img-' + i);
-            img.src = data.results[i].multimedia[2].url;
-            img.alt = (data.results[i].multimedia[2].caption > 100 ? data.results[i].multimedia[2].caption : data.results[i].title);
+            const img = document.querySelector(`#img-${i}`);
+            img.src = result.multimedia[2].url;
+            img.alt = result.multimedia[2].caption.length > 100 ? result.multimedia[2].caption : result.title;
 
-            let abstract = document.querySelector('#abstract-' + i);
-            (data.results[i].abstract.length > 150 ? abstract.innerText = data.results[i].abstract.substring(0, 100) + '...' : abstract.innerText = data.results[i].abstract);
-            date.classList.add('d-none');
-            document.querySelector('#sectionTitle').innerHTML = `<h1>${data.results[i].section}</h1>`;
+            const abstract = document.querySelector(`#abstract-${i}`);
+            abstract.innerHTML = result.abstract.length > 100 ? `${result.abstract.substring(0, 80)}...` : result.abstract;
         }
-    };
+    });
 
-    let output = '<div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 mt-3">';
-    for (let i = 7; i < data.results.length; i++) {
-        output += `<div class="col mb-3">
-                        <div class="card h-100"> 
-                            <img src="${(data.results[i].multimedia[2].url ? data.results[i].multimedia[2].url : '')}" class="card-img-top" alt="${(data.results[i].multimedia[1].caption > 100 ? data.results[i].multimedia[1].caption : data.results[i].title)}"> 
-                            <span class="text-secondary text-center"><small>${data.results[i].multimedia[2].copyright}</small></span>
-                            <div class="card-body">
-                                <a href='${data.results[i].url}'>
-                                    <h5 class="card-title fw-bold">${data.results[i].title}</h5>
-                                </a>
-                                <p class="card-text">${data.results[i].abstract}</p>
-                            </div>
-                        </div>
-                    </div>`;
-    }
-    output += '</div>';
-    dataArticle.innerHTML = output;
+    // to create article cards
+    const createArticleCard = (article) => `
+    <div class="col mb-3">
+        <div class="card h-100">
+            <img src="${article.multimedia[2]?.url || ''}" class="card-img-top" alt="${article.multimedia[1]?.caption?.length > 100 ? article.multimedia[1].caption : article.title}">
+            <span class="text-secondary text-center"><small>${article.multimedia[2]?.copyright || ''}</small></span>
+            <div class="card-body">
+                <a href='${article.url}'>
+                    <h5 class="card-title fw-bold">${article.title}</h5>
+                </a>
+                <p class="card-text">${article.abstract}</p>
+            </div>
+        </div>
+    </div>
+    `;
+
+    const generateArticleGrid = (data) => {
+        const articleCards = data.results.slice(7).map(createArticleCard).join('');
+        return `
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 mt-3">
+            ${articleCards}
+        </div>
+        `;
+    };
+    dataArticle.innerHTML = generateArticleGrid(data);
 };
 
 // display current date
@@ -62,45 +67,62 @@ const displayDate = () => {
     date.innerHTML = `<p class="fs-5">${currentDate}</p>`;
 };
 
+const truncateText = (text, maxLength) =>
+    text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+
 // function to get top articles for home page
 const getTopStoriesHome = (data) => {
+    // console.log(data.results[0].section)
     // show article in the main
-    for (let i = 0; i < 7; i++) {
-        // console.log(data.results[i].section);
-        let newsTitle = document.querySelector('#newsTitle-' + i);
-        newsTitle.href = data.results[i].url;
-        newsTitle.innerText = data.results[i].title;
+    data.results.slice(0, 7).forEach((result, i) => {
+        const newsTitle = document.querySelector(`#newsTitle-${i}`);
+        newsTitle.href = result.url;
+        newsTitle.innerHTML = result.title;
 
-        document.querySelector('.titleSection-' + i).innerText = data.results[i].section.toUpperCase();
-        let img = document.querySelector('#img-' + i);
-        img.src = data.results[i].multimedia[0].url;
-        img.alt = (data.results[i].multimedia[0].caption > 100 ? data.results[i].multimedia[0].caption : data.results[i].title);
-        document.querySelector('#copyright-' + i).innerText = data.results[i].multimedia[0].copyright;
-        let abstract = document.querySelector('#abstract-' + i);
-        (data.results[i].abstract.length > 150 ? abstract.innerText = data.results[i].abstract.substring(0, 100) + '...' : abstract.innerText = data.results[i].abstract);
+        const titleSection = document.querySelector(`.titleSection-${i}`);
+        titleSection.innerHTML = result.section;
+
+        const img = document.querySelector(`#img-${i}`);
+        img.src = result.multimedia[0].url;
+        img.alt = result.multimedia[0].caption.length > 100 ? result.multimedia[0].caption : result.title;
+
+        const copyright = document.querySelector(`#copyright-${i}`);
+        copyright.innerHTML = result.multimedia[0].copyright;
+        // console.log(copyright);
+
+        const abstract = document.querySelector(`#abstract-${i}`);
+        abstract.innerHTML = truncateText(result.abstract, 70);
+        console.log(truncateText(result.abstract, 70));
+
+    });
+
+    // to create article cards
+    const createArticleCard = (article) => `
+    <div class="col mb-3">
+        <div class="card h-100">
+            <img src="${article.multimedia[1].url}" class="card-img-top" alt="${article.multimedia[0]?.caption?.length > 100 ? article.multimedia[1].caption : article.title}">
+            <span class="text-secondary text-center"><small>${article.multimedia[2]?.copyright || ''}</small></span>
+            <div class="card-body">
+                <a href='${article.url}'>
+                    <h5 class="card-title fw-bold">${article.title}</h5>
+                </a>
+                <p class="card-text">${article.abstract}</p>
+            </div>
+            <div class="card-footer">
+            <small class="text-body-secondary">${article.byline}</small>
+            </div>
+        </div>
+    </div>
+    `;
+    const generateArticleGrid = (data) => {
+        const articleCards = data.results.slice(7).map(createArticleCard).join('');
+        return `
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 mt-3">
+            ${articleCards}
+        </div>
+        `;
     };
-
-    // show article in card
-    let output = '<div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 mt-3">';
-    for (let i = 7; i < data.results.length; i++) {
-        output += `<div class="col mb-3">
-                        <div class="card h-100">
-                            <img src="${data.results[i].multimedia[1].url}" class="card-img-top" alt="${(data.results[i].multimedia[0].caption > 100 ? data.results[i].multimedia[0].caption : data.results[i].title)}">
-                            <span class="text-secondary text-center"><small>${data.results[i].multimedia[1].copyright}</small></span>
-                            <div class="card-body">
-                            <a href='${data.results[i].url}'>
-                                <h5 class="card-title fw-bold">${data.results[i].title}</h5>
-                            </a>
-                            <p class="card-text">${data.results[i].abstract}</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-body-secondary">${data.results[i].byline}</small>
-                            </div>
-                        </div>
-                    </div>`;
-    }
-    output += '</div>';
-    dataArticle.innerHTML = output;
+    dataArticle.innerHTML = generateArticleGrid(data);
 };
 
 // set valuable for api key
@@ -115,9 +137,9 @@ fetch(url)
         getNytSection(data);
     });
 
-articleSection.addEventListener('change', (e) => {
+articleSection.addEventListener('change', e => {
 
-    let section = e.target.value;
+    const section = e.target.value;
 
     articleUrl = `https://api.nytimes.com/svc/news/v3/content/all/${section}.json?api-key=${api_key}`;
     fetch(articleUrl)
